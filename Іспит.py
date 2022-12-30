f78fe3e1eae18498f3ee103949a82b975ce67800
sortq <120167831+sortq@users.noreply.github.com>
2022-12-30 11:20:57 +0200

    Update Іспит.py

--- Іспит.py
+++ Іспит.py
@@ -1,13 +1,28 @@
-a = open("file.txt",'r',encoding='utf8')
-word = input("Введіть шукаєме слово: ")
-splited = f.read().split('\n\n')
-c = 0
-for i in splited:
-    for b in i.split():
+def edit_text():
+    with open("text.txt", "r") as file:
+        data = file.readlines()
+        for i1, i in enumerate(data):
+            if "\n" in i:
+                data[i1] = i[:-1]
+        return data
 
-        if b == tegword:
-            print("Потрібний абзац: "+ i +"\n")
-            c += 1
 
-if c == 0:
-    print("Слово не знайдено.")
+def result(text, a):
+    for count, time in enumerate(text):
+        text[count] = time.split(" ")
+    for i in text:
+        if int(i[1]) > a:
+            print(f"час - {i[0]} параметр - {i[1]}")
+
+
+def main():
+    while True:
+        parametr = input("Введіть параметр AAA -> ")
+        if parametr.isdigit():
+            result(edit_text(), int(parametr))
+            break
+        else:
+            print("Потрібно ввести цифри,спробуйте ще раз.\n")
+
+
+main()
